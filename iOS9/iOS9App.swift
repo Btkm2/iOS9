6f6bd78a1b369b88e997beaf16c4807394d22b83
Btkm2 <beketmuratbek57@gmail.com>
2023-06-10 13:08:32 +0600

    Initial Commit

---
+++ iOS9/iOS9App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS9App.swift
+//  iOS9
+//
+//  Created by Beket Muratbek on 10.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
